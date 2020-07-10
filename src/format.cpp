01d96af6416929747fa699a2adb68cdac357f22a
Hitesh Kumar <Hitesh.kumar@students.iiit.ac.in>
2020-07-10 20:24:45 +0530

    Time Bug Fixed

--- src/format.cpp
+++ src/format.cpp
@@ -1,20 +1,27 @@
 #include <string>
-
+#include <iomanip>
+#include <sstream>
 #include "../include/format.h"
 
 using std::string;
 using std::to_string;
+using std::setw;
+using std::setfill;
+
 
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 string Format::ElapsedTime(long seconds) {
     // DONE: Converted the Long seconds to given format; 
-    string res="";
+    // string res="";
+    std::ostringstream res;
     long hrs=seconds/3600;
     seconds%=3600;
     long minutes=seconds/60;
     seconds%=60;
-    res+=std::to_string(hrs)+":"+std::to_string(minutes)+":"+std::to_string(seconds);
-     return res;
+    res<<setfill('0')<<setw(2)<<std::to_string(hrs)<<":"<<
+    setfill('0')<<setw(2)<<std::to_string(minutes)<<":"<<
+    setfill('0')<<setw(2)<<std::to_string(seconds);
+     return res.str();
  }
